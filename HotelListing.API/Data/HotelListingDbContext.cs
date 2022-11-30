b81cef4d578a504d4a819b7c33a3cfadb9caca6c
Luis Banda <lbanda@choiceworx.com>
2022-11-30 01:12:00 -0600

    Added Entity Framework  and Created Databased

---
+++ HotelListing.API/Data/HotelListingDbContext.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.API.Data
+{
+    public class HotelListingDbContext: DbContext
+    {
+        public HotelListingDbContext(DbContextOptions options): base(options)
+        {
+
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasData(
+                new Country
+                {
+                    Id = 1,
+                    Name= "Jamaica",
+                    ShortName = "JMC"
+                },
+                new Country
+                {
+                    Id = 2,
+                    Name = "Bahamas",
+                    ShortName = "BS"
+                },
+                new Country
+                {
+                    Id = 3,
+                    Name = "Cayman Island",
+                    ShortName = "CI"
+                }
+                );
+
+            modelBuilder.Entity<Hotel>().HasData(
+                 new Hotel
+                 {
+                     Id = 1,
+                     Name = "Sandal Resort and Spa",
+                     Address = "Negril",
+                     CountryId = 1,
+                     Rating = 4.5
+                 },
+                 new Hotel
+                 {
+                     Id = 2,
+                     Name = "Comfort Suited",
+                     Address = "Geroge Town",
+                     CountryId = 3,
+                     Rating = 4.3
+                 },
+                 new Hotel
+                 {
+                     Id = 3,
+                     Name = "Grand Palladium",
+                     Address = "Nassua",
+                     CountryId = 2,
+                     Rating = 3.0
+                 }
+                );
+
+
+        }
+    }
+}
